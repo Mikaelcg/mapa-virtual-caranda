796f8b7810e5a295e9b078013919f83d441627c7
Mikael <mikael.gallucci@escriba.com.br>
2022-06-17 17:56:14 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
 import "photo-sphere-viewer/dist/plugins/markers.css";
 import "./assets/scss/global.scss";
 
 const app = createApp(App);
 
+app.use(PrimeVue);
 app.mount("#app");
